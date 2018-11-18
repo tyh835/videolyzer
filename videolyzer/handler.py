ff4e4834f98bdc67f4288e2846e59a6c885a7729
Tony Han <itony9401@live.com>
2018-11-18 01:19:38 -0600

    added processing API call

--- videolyzer/handler.py
+++ videolyzer/handler.py
@@ -1,3 +1,27 @@
+import urllib
+import boto3
+
 def start_processing_video(event, context):
-    print(event)
+    for record in event['Records']:
+        bucket_name = record['s3']['bucket']['name']
+        key = urllib.parse.unquote_plus(record['s3']['object']['key'])
+
+        start_label_detection(bucket_name, key)
+
     return event
+
+
+def start_label_detection(bucket_name, key):
+    rekcognition = boto3.client('rekcognition')
+    response = rekcognition.start_label_detection(
+        Video={
+            'S3Object': {
+                'Bucket': bucket_name,
+                'Name': key
+            }
+        }
+    )
+
+    print(response)
+
+    return
