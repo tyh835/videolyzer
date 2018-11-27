55a9b1ed7e0639ab682f0ba1d9c3bd43d384394d
Tony Han <itony9401@live.com>
2018-11-27 13:38:39 -0800

    updated upload script

--- upload.py
+++ upload.py
@@ -4,32 +4,26 @@ import click
 import boto3
 from botocore.exceptions import ClientError
 
-@click.group()
-def cli():
-    """Command group for Click"""
-    pass
-
-
-@cli.command('')
-@click.argument('path_name', type=click.Path(exists=True))
+@click.command()
 @click.argument('bucket_name')
+@click.argument('file_path', type=click.Path(exists=True))
 @click.option('--profile', default=None, help='Specify the AWS profile to use as credentials.')
-def upload(path_name, bucket_name, **kwargs):
-    """Upload <PATH> to <BUCKET_NAME>"""
+def upload(bucket_name=None, file_path=None, **kwargs):
+    """Upload to <BUCKET_NAME> <FILE_PATH>"""
     params = {k:v for k, v in kwargs.items() if v is not None}
     session = boto3.Session(**params)
     s3 = session.resource('s3')
 
     try:
         bucket = s3.Bucket(bucket_name)
-        path = Path(path_name).expanduser().resolve()
+        path = Path(file_path).expanduser().resolve()
         bucket.upload_file(str(path), str(path.name))
 
     except ClientError as err:
-        print('Unable to upload file {0}. '.format(path_name) + str(err) + '\n')
+        print('Unable to upload file {0}. '.format(file_path) + str(err) + '\n')
 
     return
 
 
 if __name__ == '__main__':
-    cli()
+    upload()
